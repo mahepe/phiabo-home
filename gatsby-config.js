5f908485b343f4dc2dd524392a4aa055f6148251
Matias Heikkilä <matias.heikkila@futurice.com>
2019-03-22 15:12:47 +0200

    Text stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
+        title: `Phiabo`,
+        description: `Miltä yrityksesi liiketoiminta näyttää vuonna 2025?`,
+        author: ``
     },
     plugins: [
         `gatsby-plugin-sass`,
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,73 +6,91 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Logo from "../images/logo.svg"
 import Settings from "../images/settings.svg"
+import Brain from "../images/brain.svg"
+import Cloud from "../images/cloud.svg"
+import { useStaticQuery, graphql } from "gatsby"
 
-const ListItem = (img_src, description) => (
+const ListItem = (svg, description) => (
     <div className="col-md-4 text-center">
-      <Settings className="highlight m-3" style={{width: '100px'}} />
+      {svg}
       <p>
-        {description}
+        <b>{description}</b>
       </p>
     </div>
 );
 
-const IndexPage = () => (
-    <Layout>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <div id="banner_base">
-      <div className="diagonal_cut">
-      <div className="p-4 text-left">
-      <Logo className="navbar-brand highlight" />
-      </div>
-      </div>
-          <div id="banner" className="align-items-center d-flex justify-content-center">
-        <Container>
-          <p className="banner_text">Miltä yrityksesi liiketoiminta näyttää
-            vuonna 2025?</p>
-        </Container>
-        </div>
-        <div className="diagonal_cut_2">
+
+const IndexPage = () => {
+    const { site } = useStaticQuery(
+        graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+    );
+
+    return(
+        <Layout>
+          <SEO title="Home" keywords={site.siteMetadata.keywords} />
+          <div id="banner_base">
+            <div className="diagonal_cut">
+              <div className="p-4 text-left">
+                <Logo className="navbar-brand highlight" />
+              </div>
+            </div>
+            <div id="banner" className="align-items-center d-flex justify-content-center">
+              <Container>
+                <p className="banner_text">{site.siteMetadata.description}</p>
+              </Container>
+            </div>
+            <div className="diagonal_cut_2">
+            </div>
           </div>
-      </div>
-      <Container className="p-5">
-        <div>
-          <h2>
-            Autamme sinua ottamaan tuottavuusloikan
-          </h2>
-          <p className="mt-5">
-            Cubilia suspendisse facilisi cum vitae torquent convallis justo
-            odio. Neque torquent neque enim quisque laoreet congue semper auctor
-            justo et sapien. Morbi ultrices facilisi cras? Tempus mi non sit
-            malesuada scelerisque rhoncus euismod penatibus nec! Ultrices litora
-            nec non facilisi sed ut nullam massa ipsum primis duis feugiat!
-            Libero dolor auctor posuere nisi id cursus cursus. Rhoncus lacus
-            dictumst mattis parturient erat lacinia morbi. Habitasse nec
-            torquent dolor primis posuere.
-          </p>
-        </div>
-        <Row className="mt-5 mb-5">
-          {ListItem("https://uudenmaanseuramatkat.fi/wp-content/uploads/2018/11/placeholder.png",
-                    "Tekoäly")}
-          {ListItem("https://uudenmaanseuramatkat.fi/wp-content/uploads/2018/11/placeholder.png",
-                    "Pilvipalvelut")}
-          {ListItem("https://uudenmaanseuramatkat.fi/wp-content/uploads/2018/11/placeholder.png",
-                    "Integraatiot")}
-        </Row>
-        <div className="spacer">
-        </div>
-        <div>
-          <h2>
-            Purus sit sapien
-          </h2>
-          <p className="m-0">
-            ultrices@phiabo.com
-          </p>
-          <p>
-            +358 44 000 0000
-          </p>
-        </div>
-        </Container>
-    </Layout>
-);
+          <Container className="p-5">
+            <div>
+              <h2>
+                Autamme sinua ottamaan tuottavuusloikan
+              </h2>
+              <p className="mt-5">
+                Olemme kokeneita IT-alan ammattilaisia, valmiina etsimään
+                kanssasi ratkaisuja laajan kirjoon liiketoiminnan
+                haasteita. Oli kyseessä sitten rutiininomaisten tehtävien
+                automatisointi ohjelmistorobotiikan ja tekoälyn keinoin tai
+                modernin pilvialustan käyttöönotto, olemme kumppanisi, kun
+                olet valmis saavuttamaan digitaalisten valmiuksien uuden
+                tason. Miltä yrityksesi liiketoiminta näyttää vuonna 2025? Mitä
+                jos saavuttaisit sen jo paljon aiemmin?
+              </p>
+            </div>
+            <Row className="mt-5 mb-5">
+              {ListItem((<Brain className="highlight m-3" style={{width: '100px'}} />),
+                        "Tekoäly")}
+              {ListItem((<Cloud className="highlight m-3" style={{width: '100px'}} />),
+                        "Pilvipalvelut")}
+              {ListItem((<Settings className="highlight m-3" style={{width: '100px'}} />),
+                        "Integraatiot")}
+            </Row>
+            <div className="spacer">
+            </div>
+            <div>
+              <h2>
+                Ota yhteyttä
+              </h2>
+              <p className="m-0">
+                ultrices@phiabo.com
+              </p>
+              <p>
+                +358 44 000 0000
+              </p>
+            </div>
+          </Container>
+        </Layout>
+    );};
 
 export default IndexPage;
